3c940c497d5816e383967e1e8473f35a450205ae
V-Jgenti <v.jgenti.exeedteam@gmail.com>
2021-10-11 14:24:06 +0400

    feature: winston logger integration

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-
+import { WinstonModule } from 'nest-winston';
+import * as winston from 'winston';
+import { level, levels, format, transports } from './logger/index';
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    WinstonModule.forRoot({
+      level: level(),
+      levels,
+      format,
+      transports,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/logger/index.ts
@@ -0,0 +1,43 @@
+import * as winston from 'winston';
+
+const level = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const isDevelopment = env === 'development';
+  return isDevelopment ? 'debug' : 'warn';
+};
+
+const levels = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  http: 3,
+  debug: 4,
+};
+
+const colors = {
+  error: 'red',
+  warn: 'yellow',
+  info: 'green',
+  http: 'magenta',
+  debug: 'blue',
+};
+
+winston.addColors(colors);
+const format = winston.format.combine(
+  winston.format.timestamp({ format: 'MM-DD HH:mm' }),
+  winston.format.colorize({ all: true }),
+  winston.format.printf(
+    (info) => `${info.timestamp} ${info.level}: ${info.message}`,
+  ),
+);
+
+const transports = [
+  new winston.transports.Console(),
+  new winston.transports.File({
+    filename: 'logs/error.log',
+    level: 'error',
+  }),
+  new winston.transports.File({ filename: 'logs/all.log' }),
+];
+
+export { level, levels, format, transports };
